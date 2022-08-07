9f46f28ba8d683f16814c3e86d13b51b9f43be4e
lxhillwind <lxhillwind@qq.com>
2022-08-07 10:22:24 +0800

    tweak markdown title gen

--- blog/build.sh
+++ blog/build.sh
@@ -5,11 +5,9 @@ set -e
 # e.g. (run it in background)
 # podman run --name pandoc --init --rm -dt --entrypoint= docker.io/pandoc/core tail -f
 
-podman exec -i pandoc pandoc -r gfm -w html --embed-resources --standalone --metadata title='blog index' < index.md > index.html
+podman exec -i pandoc pandoc -r markdown+east_asian_line_breaks -w html --standalone < index.md > index.html
 sed -E -i 's/href="(.*).md"/href=".\/\1.html"/' index.html
 
-for i in $(\ls | grep -E '^[0-9]+.*.md$'); do
-    title=${i%.md}
-    title=${title#*.}
-    podman exec -i pandoc pandoc -r gfm -w html --embed-resources --standalone --metadata title="$title" < "$i" > "${i%.md}.html"
+for i in $(sed -nE 's/^- \[.*\(([0-9]+.*.md)\)$/\1/p' index.md); do
+    podman exec -i pandoc pandoc -r markdown+east_asian_line_breaks -w html --mathjax --standalone < "$i" > "${i%.md}.html"
 done
--- blog/index.md
+++ blog/index.md
@@ -1,7 +1,9 @@
+% blog index
+
 <!-- vim:fdm=marker {{{
 # collect files
-:exe 'norm 4j"_dG' | Cdb Sh -r git ls-files | grep -E '^[0-9]+' | sort -n -r
-# generate link (TODO gen title)
-:exe 'norm 2j' | :.,$s/\v^(\d+)\.(.*).md$/- [\1. \2](\1.\2.md)/
+:exe 'norm 4j"_dG' | Cdb Sh -r git ls-files | grep -E '^[0-9]+' | sort -n -r | xargs -r -I {} sh -c 'printf "%s/" "$1"; head -n 1 "$1"' -- {}
+# generate link
+:exe 'norm 2j' | :.,$s/\v^(\d+)\.([^\/]+)\/\%\s*(.+)$/- [\3](\1.\2)/
 }}} -->
-- [0. test](0.test.md)
+- [test](0.test.md)
