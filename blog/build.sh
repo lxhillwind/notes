921d7d930cda906a1eb94743d5145990f7184831
lxhillwind <lxhillwind@qq.com>
2022-08-09 22:23:52 +0800

    move index number before title

--- blog/build.sh
+++ blog/build.sh
@@ -5,15 +5,15 @@ set -e
 # e.g. (run it in background)
 # podman run --name pandoc --init --rm -dt --entrypoint= docker.io/pandoc/core tail -f
 
-podman exec -i pandoc pandoc -r markdown+east_asian_line_breaks -w html --standalone -T 'lxhillwind' < index.md > index.html
-sed -E -i 's/href="(.*).md"/href=".\/\1.html"/' index.html
-
 # put some file into container.
 for i in res-*.html; do
     podman exec -i pandoc sh -c "cat > $i" < "$i"
 done
 
-for i in $(sed -nE 's/^- \[.*\(([0-9]+.*.md)\)$/\1/p' index.md); do
+podman exec -i pandoc pandoc -r markdown+east_asian_line_breaks -w html --standalone -T 'lxhillwind' -H res-icon.html < index.md > index.html
+sed -E -i 's/href="(.*).md"/href=".\/\1.html"/' index.html
+
+for i in $(sed -nE 's/^- .*\[.*\(([0-9]+.*.md)\)$/\1/p' index.md); do
     # -H: --include-in-header=FILE
     # -B: --include-after-body=FILE
     # -A: --include-after-body=FILE
